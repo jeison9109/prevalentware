8ddca7a62a013198e78ec0fd61ffe50ceab01a08
Yeison ivan Serna lopez <yeisonivanserna@gmail.com>
2021-12-03 21:39:57 -0500

    add test data

---
+++ data/companies.js
@@ -0,0 +1,143 @@
+module.exports = [
+  {
+    docNumber: "1231234",
+    name: "Prevalent",
+    businessName: "PrevalentWare",
+    type: "NIT",
+    numEmployees: 10,
+    logo: "http://localhost:3000/images/prevalent.png",
+    status: "pending",
+    documents: {
+      create: [
+        {
+          name: "RUT PrevlalentWare",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Logo PrevlalentWare",
+          url: "http://localhost:3000/images/prevalent.png",
+        },
+        {
+          name: "Acta de constitución PrevlalentWare",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Cámara de Comercio Prevalentwaree",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Otro Documento  Prevalentware",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+      ],
+    },
+  },
+  {
+    docNumber: "45456756",
+    name: "PVR S.A.S",
+    businessName: "Petroleos asociados",
+    type: "AT",
+    numEmployees: 20,
+    logo: "http://localhost:3000/images/pvr.png",
+    status: "pending",
+    documents: {
+      create: [
+        {
+          name: "Petroleos asociados",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Logo PVR S.A.S",
+          url: "http://localhost:3000/images/pvr.png",
+        },
+      ],
+    },
+  },
+  {
+    docNumber: "34446568",
+    name: "Adidas",
+    businessName: "Adidasware",
+    type: "NIT",
+    numEmployees: 350,
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1024px-Adidas_Logo.svg.png",
+    status: "pending",
+    documents: {
+      create: [
+        {
+          name: "RUT Adidas",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Logo Adidas",
+          url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1024px-Adidas_Logo.svg.png",
+        },
+        {
+          name: "Acta de constitución Adidas",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Cámara de Comercio Adidas",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Otro Documento Adidas",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+      ],
+    },
+  },
+  {
+    docNumber: "3432446566",
+    name: "Nike",
+    businessName: "Nikeware",
+    type: "ID",
+    numEmployees: 400,
+    logo: "https://logos-marcas.com/wp-content/uploads/2020/04/Nike-Logo.png",
+    status: "pending",
+    documents: {
+      create: [
+        {
+          name: "Nike",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Logo Nike",
+          url: "https://logos-marcas.com/wp-content/uploads/2020/04/Nike-Logo.png",
+        },
+      ],
+    },
+  },
+  {
+    docNumber: "3453465456",
+    name: "Apple",
+    businessName: "Appleware",
+    type: "AT",
+    numEmployees: 5000,
+    logo: "https://logos-marcas.com/wp-content/uploads/2020/04/Apple-Logo.png",
+    status: "pending",
+    documents: {
+      create: [
+        {
+          name: "RUT Apple",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Logo Apple",
+          url: "https://logos-marcas.com/wp-content/uploads/2020/04/Apple-Logo.png",
+        },
+        {
+          name: "Acta de constitución Apple",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Cámara de Comercio Apple",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+        {
+          name: "Otro Documento Apple",
+          url: "http://localhost:3000/docs/test_doc.pdf",
+        },
+      ],
+    },
+  },
+];
